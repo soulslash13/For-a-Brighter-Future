654816e00e444ee5f4a0e879cc322aae2b13d341
Xander Shoup <xtshoup@windstream.net>
2017-02-16 09:14:40 -0500

    You spin me right round-
    Makes round things spin!

---
+++ Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	//This script causes objects to rotate along their center point
+	//For windmills, the speed should be negative (unless you mirror the windmill)
+	//For waterwheels, the speed should be positive (unless you mirror it)
+	public Rigidbody2D rb;
+	public int speed;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddTorque(speed);
+	}
+}
