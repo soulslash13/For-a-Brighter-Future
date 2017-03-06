f7f04ef7664b453bbfd11eab5d98b6c7e10e63dc
Xander Shoup <xtshoup@windstream.net>
2017-03-06 09:38:05 -0500

    Wobbly

---
+++ Scripts/Wobble.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wobble : MonoBehaviour {
+
+public Rigidbody2D rb;
+public int multiplier;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce(Random.insideUnitCircle * multiplier);
+	}
+}
