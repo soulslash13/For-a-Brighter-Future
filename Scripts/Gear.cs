016f2c9212c7473ebca9da8831ef5dc4a0f795d5
Xander Shoup <xtshoup@windstream.net>
2017-04-03 09:20:25 -0400

    Fsshwing
    I can throw gears now. Can you throw gears now?

---
+++ Scripts/Gear.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gear : MonoBehaviour {
+private Transform tf;
+public float speed;
+public float rotSpeed;
+public int dir = 0; //0 up, 1 right, 2 down, 3 left
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tf.Rotate(0,0,rotSpeed);
+		if(dir==0){
+			tf.position += Vector3.up *speed;
+		}
+		if(dir==1){
+			tf.position += Vector3.right *speed;
+		}
+		if(dir==2){
+			tf.position += Vector3.up *-speed;
+		}
+		if(dir==3){
+			tf.position += Vector3.right *-speed;
+		}
+	}
+}
